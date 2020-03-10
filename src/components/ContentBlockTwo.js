f5e9b5109651d7dd71881d860079adcaa10b7703
Nicholas Hinzman <nicholashinzman@Nicholass-MacBook-Pro.local>
2020-03-10 07:12:41 -0500

    h2

--- src/components/ContentBlockTwo.js
+++ src/components/ContentBlockTwo.js
@@ -6,16 +6,16 @@ import {safePrefix, markdownify, Link} from '../utils';
 export default class ContentBlockTwo extends React.Component {
     render() {
         return (
-            <section id={_.get(this.props, 'section.section_id')} className="block text-block outer">
+            <section id={_.get(this.props, 'section.section_id')} className="block text-block outer two-column-content">
               <h2 className="block-title">{_.get(this.props, 'section.title')}</h2>
               <div className="inner">
                 <div className="block-inside">
-                  <div className="block-content">
+                  <div className="block-content left">
                     <div className="block-text">
                       {markdownify(_.get(this.props, 'section.content'))}
                     </div>
                   </div>
-                  <div className="block-content">
+                  <div className="block-content right">
                     <div className="block-text">
                       {markdownify(_.get(this.props, 'section.content_two'))}
                     </div>
