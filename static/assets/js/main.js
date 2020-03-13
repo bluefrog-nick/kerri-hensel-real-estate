23d7cc641e0dca78f169055d7f4725b67926a1fb
Nicholas Hinzman <nicholashinzman@Nicholass-MBP.home.gateway>
2020-03-13 11:21:29 -0500

    sadf

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -39,6 +39,7 @@ if (submenu) {
   }
 
 }
+!function(e,n){function t(e){const t=function(e){const t=n.createElement("link");return t.rel="prerender",t.href=e,t}(e);n.head.appendChild(t)}function r(e){e.removeEventListener("mouseenter",o,!1)}function o(o){const c=o.target,{href:i}=c,u=new URL(i),f=u.href;!function(e){return!!n.head.querySelector(`link[rel="prerender"][href="${e}"]`)}(f)&&function(n){const{host:t}=e.location;return n.host===t}(u)?(t(f),r(c)):r(c)}function c(e){e.addEventListener("mouseenter",o,!1)}n.querySelectorAll("a").forEach(c)}(window,window.document);
 
 let docsNav = document.getElementById('docs-nav');
 if (docsNav) {
